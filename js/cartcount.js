35d5d796658201277aa847e30ca8f38c265c6f05
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-14 11:20:00 +0545

    first commit

---
+++ js/cartcount.js
@@ -0,0 +1,6 @@
+let cartCount = 0;
+
+function addtocart() {
+    cartCount++; // Increment the cart count
+    document.getElementById("cart-count").textContent = cartCount; // Update the cart icon number
+}

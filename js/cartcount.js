c207af3f64500107777e88fb15e57107a9d62dcb
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-15 20:41:11 +0545

    push night

--- js/cartcount.js
+++ js/cartcount.js
@@ -1,19 +1,11 @@
 const addButton = document.getElementById("addtocart");
 
-// from product file 
+const productName = document.getElementById("product_name");
 
-const productName = document.getElementById('product_name')
-const productPrice = document.getElementById('product_price');
-
-// from checkout file
-const checkId = document.getElementById("check_id");
 const checkName = document.getElementById("check_name");
 
-const checkPrice = document.getElementById("check_price");
-const checkQuantity = document.getElementById("check_quantity");
-const checkTotal = document.getElementById('check_total');
 
-// from checkot file 
+
 
 
 
@@ -24,18 +16,19 @@ addButton.addEventListener ('click', () =>
     {
         count++; // Increment the number
         document.getElementById('cartCount').innerText = count; // Update the displayed number
+         
+        localStorage.setItem('countNumber', count)
+
+
+
+        const pName = productName.textContent;
+        localStorage.setItem('productName', pName);
+
+        // pName.innerHTML = checkName;
+
+
+
     
-        const checkidValue = checkId.textContent;
-
-        const productnameValue = productName.textContent;
-        checkName.innerText = productnameValue;
-
-
-        localStorage.setItem('productName',productnameValue)
-
-
-
-
     
     }
 

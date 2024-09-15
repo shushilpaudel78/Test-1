cefedd213002fcb6cbf3dcb8bd8d2961579c4bad
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-15 21:42:26 +0545

    push code

--- js/cartcount.js
+++ js/cartcount.js
@@ -1,47 +1,11 @@
-const addButton = document.getElementById("addtocart");
 
-const productName = document.getElementById("product_name");
-
-const checkName = document.getElementById("check_name");
-
-
-
-
-
-
-let count = 0;
-
-// Function to increment the number
-addButton.addEventListener ('click', () =>
-    {
-        count++; // Increment the number
-        document.getElementById('cartCount').innerText = count; // Update the displayed number
-         
-        localStorage.setItem('countNumber', count)
-
-
-
-        const pName = productName.textContent;
-        localStorage.setItem('productName', pName);
-
-        // pName.innerHTML = checkName;
-
-
-
-    
-    
-    }
-
-)
-
-
-
-
-
-
-
-
-
+const AddtoStorage = (id, name, price) => {
+    const product = {id:id, name:name, price:price}
+    let storedProducts = JSON.parse(localStorage.getItem('products')) || [];
+    storedProducts.push(product);
+    localStorage.setItem('products', JSON.stringify(storedProducts));
+    document.getElementById('cartCount').innerText = storedProducts.length;
+}
 
 
 

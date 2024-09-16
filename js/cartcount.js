fbff9df3dfafbb695b50def2c21191daad7da2a7
Shushil <84438191+shushilpaudel78@users.noreply.github.com>
2024-09-16 17:52:57 +0545

    test

--- js/cartcount.js
+++ js/cartcount.js
@@ -1,11 +1,38 @@
 
+// For save data in local storage and add number of cart in nav 
+
 const AddtoStorage = (id, name, price) => {
-    const product = {id:id, name:name, price:price}
+    const product = { id: id, name: name, price: price }
     let storedProducts = JSON.parse(localStorage.getItem('products')) || [];
     storedProducts.push(product);
     localStorage.setItem('products', JSON.stringify(storedProducts));
     document.getElementById('cartCount').innerText = storedProducts.length;
+
+
+    let productNames = storedProducts.map(product => product.name).join(', ');
+    document.getElementById('check_name').innerText = productNames;
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
